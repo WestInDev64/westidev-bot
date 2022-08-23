b6c65c091741d6bcf7d17da7dfbd713ba29e2fbb
Axel Labarre <axel.labarre@gmail.com>
2022-08-23 20:36:54 +0200

    role-claim -> Mise à jour roles , emojis pour Webmonster

--- functions/role-claim.js
+++ functions/role-claim.js
@@ -6,22 +6,29 @@ const ROLE_CHANNEL_ID = '981193791144857651';
 
 /**
  * Récupération des customs émoji et des roles */
-const emojis = {
-    windows: 'Windows',
-    apple: 'MacOS',
-    linux: 'Linux',
-    ruby: 'Ruby',
-    rust: 'Rust',
-    php: 'PHP',
-    css: 'CSS',
-    html: 'HTML',
-    py: 'Python',
-    csharp: 'C#',
-    c_: 'C',
-    cpp: 'C++',
-    java: 'Java',
-    js: 'JavaScript',
+const emojisPro = {
+    boss: 'Chef d\'entreprise',
+    dev: 'Développeur',
+    design: 'UX/UI Designer - Graphise',
+    marketing: 'Webmarketing - SEO',
+    security: 'Cybersécurité',
+    system: 'Admin Système - DevOps',
+    po: 'Chef de projet - Scrum Master - PO',
+    formateur: 'Formateur Tech',
+    data: 'Data Analyst / Scientist / Engineer',
+    student: 'Étudiant',
 };
+
+/* const emojisGeo = {
+    Martinique: 'Martinique',
+    Guadeloupe: 'Guadeloupe',
+    Guyanne: 'Guyanne',
+    France: 'France',
+    Canada: 'Canada',
+    Suisse: 'Suisse',
+    AutresPays: 'Autres Pays',
+}; */
+
 
 /**
  * @param {MessageReaction} reaction
@@ -34,13 +41,15 @@ const handleReacts = (reaction, user, add) => {
     // on récupère le serveur
     const { guild } = reaction.message;
     // on récupère le nom du role
-    const roleName = emojis[emojiName];
+    const roleName = emojisPro[emojiName];
+
     if (!roleName) return;
     // on cherche le bon role dans le serveur
     // eslint-disable-next-line no-shadow
     const role = guild.roles.cache.find(role => role.name === roleName);
     if (!role) return;
 
+    // on récupère le membre
     // eslint-disable-next-line no-shadow
     const member = guild.members.cache.find(member => member.id === user.id);
     if (!member) return;
@@ -51,9 +60,35 @@ const handleReacts = (reaction, user, add) => {
     else {
         member.roles.remove(role);
     }
-
 };
 
+/* const handleReacts2 = (reaction, user, add) => {
+    // on récupère l'emoji
+    const emojiName = reaction.emoji.name;
+    // on récupère le serveur
+    const { guild } = reaction.message;
+    // on récupère le nom du role
+    const roleName2 = emojisGeo[emojiName];
+
+    if (!roleName2) return;
+    // on cherche le bon role dans le serveur
+    // eslint-disable-next-line no-shadow
+    const role = guild.roles.cache.find(role => role.name === roleName2);
+    if (!role) return;
+
+    // on récupère le membre
+    // eslint-disable-next-line no-shadow
+    const member = guild.members.cache.find(member => member.id === user.id);
+    if (!member) return;
+
+    if (add) {
+        member.roles.add(role);
+    }
+    else {
+        member.roles.remove(role);
+    }
+};
+ */
 
 /**
  * @param { Client } bot */
@@ -64,17 +99,29 @@ module.exports = (bot) => {
     const getEmoji = (emojiName) => bot.emojis.cache.find(emoji => emoji.name === emojiName);
     const reactions = [];
 
-    let text = 'Réagis avec l\'un des émojis pour obtenir le rôle de développeur qui te correspond le mieux : \n\n';
-    for (const key in emojis) {
+    let text = 'Réagis avec l\'un des émojis pour obtenir le rôle qui te correspond le mieux : \n\n';
+    for (const key in emojisPro) {
         const emoji = getEmoji(key);
         console.log('emoji = ' + emoji);
         if (!emoji) return;
         reactions.push(emoji);
-        text += `${emoji} : ${emojis[key]}\n`;
+        text += `${emoji} : ${emojisPro[key]}\n`;
         console.log(key);
     }
 
+    /*     let text2 = 'Sélectionne ta localisation en réagissant à l\'un des émojis suivants: \n\n';
+        for (const key in emojisGeo) {
+            const emoji = getEmoji(key);
+            console.log('emoji = ' + emoji);
+            if (!emoji) return;
+            reactions.push(emoji);
+            text2 += `${emoji} : ${emojisGeo[key]}\n`;
+            console.log(key);
+        } */
+
+
     firstMessage(channel, text, reactions);
+    //firstMessage(channel, text2, reactions);
 
     bot.on('messageReactionAdd', (reaction, user) => {
         // Si notre réaction provient de notre channel
